2e30b513894ce1f91ee3835e0bc2ff04326e262c
Sabbir Tanvir <sabbirmahmudtanvir@gmail.com>
2025-03-15 01:23:51 +0600

    heading

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,6 +12,26 @@ export const metadata: Metadata = {
   icons: {
     icon: "/favicon.svg",
   },
+  openGraph: {
+    title: "Sabbir Tanvir — Software Engineer ",
+    description: "Discover a skilled Software Engineer and UI/UX Designer, renowned for creating impactful software that achieves exceptional results.",
+    url: "https://sabbir-tanvir.netlify.app/",
+    type: "website",
+    images: [
+      {
+        url: "/dp3.jpg", // Path to image in public folder
+        width: 1200,
+        height: 630,
+        alt: "Sabbir Tanvir",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Sabbir Tanvir — Software Engineer",
+    description: "Discover a skilled Software Engineer and UI/UX Designer, renowned for creating impactful software that achieves exceptional results.",
+    images: ["/dp3.jpg"], // Already correctly referencing the public folder
+  },
 };
 
 export default function RootLayout({
