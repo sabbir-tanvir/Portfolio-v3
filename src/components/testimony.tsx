b0bf9b6b99af376224726e29ca71bd4141c2d7d8
Sabbir Tanvir <sabbirmahmudtanvir@gmail.com>
2025-03-19 00:56:10 +0600

    bug fix

--- src/components/testimony.tsx
+++ src/components/testimony.tsx
@@ -3,6 +3,12 @@ import { AnimatedTestimonials } from "@/components/ui/animated-testimonials";
 export function AnimatedTestimonialsDemo() {
   const testimonials = [
     {
+        quote: "Sabbir leads with a positive attitude and a willingness to help others. He is a great team player and is always willing to go the extra mile to help out his team.",
+        name: "Selim Reza",
+        designation: "Operations Director at CloudScale",
+        src: "/selim.jpg",
+      },
+    {
       quote:"I have worked with Sabbir on a number of projects and I have always been impressed with his design skills. He is a great team player and always brings a positive attitude to the table. I would highly recommend Sabbir for any design project.",
       name: "Rumon Khan",
       designation: "Product Manager at TechFlow",
@@ -14,12 +20,7 @@ export function AnimatedTestimonialsDemo() {
       designation: "CTO at InnovateSphere",
       src: "/rasel.png",
     },
-    {
-      quote: "Sabbir leads with a positive attitude and a willingness to help others. He is a great team player and is always willing to go the extra mile to help out his team.",
-      name: "Selim Reza",
-      designation: "Operations Director at CloudScale",
-      src: "/selim.jpg",
-    },
+
     {
       quote: "his design proficiency is remarkable, consistently delivering top-notch work that elevates user experiences. His creativity and keen eye for detail make him an admirable asset when it comes to design work.",
       name: "Emon Mia",
